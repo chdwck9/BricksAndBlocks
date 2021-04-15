eab8ebf724e1e3ade0823bef6bfbdb8582e2c884
chdwck9 <chdwck9@gmail.com>
2021-04-15 14:38:55 +0200

    Add files via upload

---
+++ test.cmd
@@ -0,0 +1,2 @@
+"C:\Users\chdwc\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\chdwck9\Ship\Assets\My Scripts\Bricks and Blocks\bricksAndBlocks.py" "C:\Users\chdwck9\Temp\Test area" "block"
+pause
---
+++ test.py
@@ -0,0 +1,3 @@
+import BricksAndBlocks.BricksAndBlocks as BB
+
+bb = BB.BricksAndBlocks(["block",r'C:\Users\chdwck9\Temp\Test area'])
